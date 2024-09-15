a0b1a56c49e27a842c712003a6dbee414e7fe71e
Seoheeda <1015aliciay@gmail.com>
2024-09-16 00:40:33 +0900

    tailwind setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Header from './components/header';
-import Home from './pages/home';
+import Header from './components/header.tsx';
+import Home from './pages/home.tsx';
 
 const App: React.FC = () => {
   return (
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary : {
+          DEFAULT : "#C8A2C8",
+          1: "#C8A2C8",
+          2: "#E6E6FA",
+        },
+        gray: {
+          DEFAULT: "#F0F0F0",
+          1: "#F0F0F0",
+          2: "#D8D8D8",
+        }
+      }
+    },
+  },
+  plugins: [],
+}
+
